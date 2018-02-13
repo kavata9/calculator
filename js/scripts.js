ec1a7fe35900e986b07496b6a20e68baf406792c
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-02-13 15:21:35 +0300

    make changes on the script to calculate a persons bmi

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var divide= function(number1, number2) {
            return number1 / number2;
          };
 
-         var number1 = parseInt(prompt("Enter a number:"));
-         var number2 = parseInt(prompt("Enter another number:"));
+         var number1 = parseInt(prompt("Enter a weight in kilograms:"));
+         var number2 = parseInt(prompt("Enter height in meters ^2:"));
 
           alert(divide(number1, number2));
